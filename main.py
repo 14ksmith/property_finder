8f1baad591f971aee80e2480a0dd06a207eb33d0
14ksmith <ksmith2017@gmail.com>
2022-07-28 17:24:14 -0400

    Made notes about how to use the firebase_admin methods.

--- main.py
+++ main.py
@@ -19,52 +19,67 @@ firebase = Firebase(
 # example firebase usage
 # ==========================================================
 
+## SET
+## Sets the data at this location to a given value. If you already have values and children created, they will be overridden
+# This will set a value directly at the route
+firebase.rtdb.set("seeds")
 
+## CHILD
+## Returns a reference to the specified child node
+# This will create a new child called "watermelon" if there is not one already, and set the value to 'seeds'
+# If there is already a child called "watermelon", it will override that child with the new once, replacing it
+input("press enter when ready\n")
+firebase.rtdb.child("watermelon").set({"watermelon": "seeds"})
+
+# This will override the old "watermelon" child, and set the value to 'rind'
+input("press enter when ready\n")
+firebase.rtdb.child("watermelon").set("rind")
+
+# This will make multiple children off the same node
+input("press enter when ready\n")
+firebase.rtdb.child("watermelon").child("big").set("huge")
+
+# Change this back to original configuration
+input("press enter when ready\n")
+firebase.rtdb.child("watermelon").set({"watermelon": "seeds"})
+
+
+## PUSH
+##ALWAYS MAKES A NEW CHILD NODE
+# Creates a new child node. Optional value argument can be used to provide an ititial value fo rhte child node,
+#       otherwise the new child will have an empty string as the default value
+# This will create a new child node with an empty string as the value
+input("press enter when ready\n")
+firebase.rtdb.push("")
+# This will create a new child node with "watermelon" as the value
+input("press enter when ready\n")
+firebase.rtdb.push("watermelon")
+# If a key is indicated with the value, then it will create the child with the specified key: value pair
+input("press enter when ready\n")
+firebase.rtdb.push({"watermelon": "seeds", "apple": "core", "grape": "skin"})
+# this will just create another child node with the following dictionary key:value pairs
+input("press enter when ready\n")
+firebase.rtdb.push({"watermelon": "rind", "apple": "seeds", "grape": "fruit"})
+
+
+## UPDATE
+# Updates the specified child keys of this reference to the provided values.
+# Needs dict. of child keys to update and their new values
+# Same as 'patch'
+firebase.rtdb.child("watermelon").update({"watermelon": "smoothie"})
+
+## GET
+# Returns the value at the current location of the database
+# This will return the value at the child named "watermelon"
+input("press enter when ready\n")
 print(firebase.rtdb.child("watermelon").get())
+
+
+## DELETE
+# This deletes the specified value or child
+input("press enter when ready\n")
 firebase.rtdb.child("watermelon").delete()
 
-
-input("press enter when ready\n")
-# equivalent to put, it overwrites the key and the value at the key.
-# if there is no key, it will make the key there
-firebase.rtdb.push("watermelon")
-
-# you can also add dictionaries, lists, numbers, strings and any other type that can be serialized into json
-firebase.rtdb.push({"key_here": "valHere"})
-
-
-firebase.rtdb.update()
-
-# input("press enter when ready\n")
-
-# lets add a child
-# firebase.rtdb.child("watermelon").push("seeds")
-
-
-firebase.rtdb.child("watermelon").child("big").set("seeds")
-
-# now this will overwrite the value at big
-firebase.rtdb.child("watermelon").child("big").set("rind")
-
-input("press enter when ready\n")
-# gets the node at the reference
-# note that this is not getting the data at the node
-# also note that you can chain children nodes
-firebase.rtdb.child("")
-
-input("press enter when ready\n")
-# now we will overwrite some data
-firebase.rtdb.push("")
-
-input("press enter when ready\n")
-# now we will get the data and print it
-firebase.rtdb.child("").get()
-
-input("press enter when ready\n")
-firebase.rtdb.get("watermelon")
-
-input("press enter when ready\n")
-firebase.rtdb.delete()
 
 # ==========================================================
 # ==========================================================
